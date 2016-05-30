b92da27892149a073fa2f435d24f2ea580d0efe9
Tom Klijn <tomklijn@outlook.com>
2016-05-30 20:09:56 +0200

    setup

---
+++ AC_Fan_Controller/AC_Fan_Controller/AC_Fan_Controller.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		AC_Fan_Controller.ino
+ Created:	5/30/2016 8:09:17 PM
+ Author:	Tom
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
